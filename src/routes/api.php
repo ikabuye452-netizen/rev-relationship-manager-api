55185fae1e417d17787089cdddd5704fbcbf5d52
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-10-26 19:11:35 +0700

    fix route management to be cleaner to read

--- src/routes/api.php
+++ src/routes/api.php
@@ -7,14 +7,42 @@ $usersController = new UsersController();
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-if ($requestUri == "/" && $requestMethod == "GET") {
-    echo "You are in the root page!";
-} elseif ($requestUri == "/users" && $requestMethod == "GET") {
-    $usersController->getAllUser();
-} elseif (preg_match('#^/users/(\d+)$#', $requestUri, $matches) && $requestMethod == "GET") {
+
+if ($requestUri == "/") {
+    switch ($requestMethod) {
+        case "GET":
+            echo "You are in the root page!";
+            break;
+        default:
+            sendResponse405() ;
+            break;
+    }
+} elseif ($requestUri == "/users") {
+    switch ($requestMethod) {
+        case "GET":
+            $usersController->getAllUser();
+            break;
+        default:
+            sendResponse405();
+            break;
+    }
+} elseif (preg_match('#^/users/(\d+)$#', $requestUri, $matches)) {
     $userId = $matches[1];
-    $usersController->getUserById($userId);
+    switch ($requestMethod) {
+        case "GET":
+            $usersController->getUserById($userId);
+            break;
+        default:
+            sendResponse405();
+            break;
+    }
 } else {
     http_response_code(404);
     echo "Error 404! No route found!";
+}
+
+
+function sendResponse405() {
+    http_response_code(405);
+    echo json_encode(["message" => "Method Not Allowed"]);
 }
